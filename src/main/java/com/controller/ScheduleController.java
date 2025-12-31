b8f77fad239f8ab2471f5e6d4c2bb93269be183e
SEO HANA <onnuri1226@gmail.com>
2025-12-31 19:14:07 +0900

    /schedulers로 통일

--- src/main/java/com/controller/ScheduleController.java
+++ src/main/java/com/controller/ScheduleController.java
@@ -17,7 +17,7 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     // 일정 생성 - 일정 제목, 일정 내용, 작성자명, 비밀번호, 작성/수정일 저장 (API 응답에는 pw 제외)
-    @PostMapping("/scheduler")
+    @PostMapping("/schedulers")
     public ResponseEntity<ScheduleCreateResponse> create (
             @RequestBody ScheduleCreateRequest request
     ) {
@@ -25,14 +25,14 @@ public class ScheduleController {
     }
 
     // 일정 전체 조회 - 작성자명 기준으로 목록 조회 (포함 안될수도), 수정일 기준 내림차순 정렬 (API 응답 pw 제외)
-    @GetMapping("/scheduler")
+    @GetMapping("/schedulers")
     public ResponseEntity<List<ScheduleGetResponse>> getAll(
             @RequestParam(required = false, defaultValue = "modifiedAt", value = "orderby") String username) {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.findAll());
     }
 
     // 일정 선택 조회 - 단건 정보 조회, 고유 식별자(ID) 사용 (API 응답 pw 제외)
-    @GetMapping("/scheduler/{scheduleId}")
+    @GetMapping("/schedulers/{scheduleId}")
     public ResponseEntity<ScheduleGetResponse> getOne(
             @PathVariable Long scheduleId
     ) {
@@ -40,7 +40,7 @@ public class ScheduleController {
     }
 
     // 일정 수정 - 선택 일정 한정 조회, 일정 제목/작성자명만 수정 가능, 비밀번호 전달 (API 응답 pw 제외)
-    @PutMapping("/scheduler/{scheduleId}")
+    @PutMapping("/schedulers/{scheduleId}")
     public ResponseEntity<ScheduleUpdateResponse> update(
             @PathVariable Long scheduleId,
             @RequestBody ScheduleUpdateRequest request
@@ -49,7 +49,7 @@ public class ScheduleController {
     }
 
     // 일정 삭제 - 선택 일적 삭제, 비밀번호 전달
-    @DeleteMapping("scheduler/{scheduleId}")
+    @DeleteMapping("/schedulers/{scheduleId}")
     public void delete(
             @PathVariable Long scheduleId
     ) {
