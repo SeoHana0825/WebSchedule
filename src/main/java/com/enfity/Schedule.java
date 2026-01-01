8c796168b0aed37e6ff231adb78b14d5d5d96c68
Seohana <onnuri1226@gmail.com>
2026-01-01 21:29:24 +0900

    스프링 베이직반 과제 피트백 수정 -테이블네임, 컨트롤러 API

--- src/main/java/com/enfity/Schedule.java
+++ src/main/java/com/enfity/Schedule.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name = "scheduler")
+@Table(name = "/schedulers")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Schedule extends BaseEntity {
 
